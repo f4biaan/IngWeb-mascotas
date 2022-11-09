493edc0e7b99490c2d4de48775bb331a73c99d07
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-11-09 05:55:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,6 +12,7 @@
 	<link rel="apple-touch-icon" href="images/logo.png" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
 </head>
+
 <body>
 	<div class="contenedor">
 		<header class="cabeceraPrincipal">
@@ -18,14 +20,20 @@
 			<nav class="menuPrincipal">
 				<a href="#">Inicio</a>
 				<a href="internas/servicios.php">Servicios</a>
-				<a href="#">Productos</a>
-				<a href="#">Adopción</a>
-				<a href="#">Contactos</a>
+				<a href="internas/productos.php">Productos</a>
+				<a href="internas/adopcion.php">Adopción</a>
+				<a href="internas/contactos.php">Contactos</a>
 			</nav>
 		</header>
-	
+
 		<section class="sliderPrincipal">
 			<img src="images/slider1.jpg">
+			<div class="info">
+				<h2>Proyecto de adopción de mascotas</h2>
+				<div class="boton-adop">
+					<button class="button button1">ADOPTANOS</button>
+				</div>
+			</div>
 		</section>
 
 		<section class="titulo">
@@ -36,8 +44,8 @@
 			<img src="images/slider2.jpg" title="Casimiro" description="perro en adopcion">
 			<section class="card1">
 				<h3 class="mayusculas colorAzul">Casimiro</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet mi nisi, vitae maximus neque laoreet sit amet. Vestibulum sed lacus eu justo convallis ultricies ut et sapien. Sed sed risus non arcu lobortis lacinia quis in elit. Duis cursus nunc a enim iaculis cursus. Nulla sed sem at turpis.</p>
-				<div class="derecha">		
+				<p class="derecha">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet mi nisi, vitae maximus neque laoreet sit amet. Vestibulum sed lacus eu justo convallis ultricies ut et sapien. Sed sed risus non arcu lobortis lacinia quis in elit. Duis cursus nunc a enim iaculis cursus. Nulla sed sem at turpis.</p>
+				<div class="derecha">
 					<button class="button button1">Mas información</button>
 				</div>
 			</section>
@@ -45,21 +53,37 @@
 
 		<section class="adopcion2">
 			<img src="images/slider3.jpg" title="garto en adopcion" description="gatp en adopcion">
-			<section class="card1">
+			<article class="card1">
 				<h3 class="mayusculas colorAzul">Thomas</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet mi nisi, vitae maximus neque laoreet sit amet. Vestibulum sed lacus eu justo zonvallis ultricies ut et sapien. Sed sed risus non arcu lobortis lacinia quis in elit. Duis cursus nunc a enim iaculis cursus. Nulla sed sem at turpis.</p>
-				<div class="derecha">		
+				<div class="derecha">
 					<button class="button button1">Mas información</button>
 				</div>
-			</section>
+			</article>
 		</section>
 
 		<section class="doctor">
-			<h3>Doctores</h3>
+			<div class="doctor-cont">
+				<div class="doc">
+					<img src="images/doctor1.png" alt="">
+				</div>
+				<div class="doc">
+					<img src="images/doctor1.png" alt="">
+				</div>
+				<div class="doc">
+					<img src="images/doctor1.png" alt="">
+				</div>
+				<div class="doc">
+					<img src="images/doctor1.png" alt="">
+				</div>
+			</div>
 		</section>
 
 		<section class="sponsor">
-			<h3>Sponsor</h3>
+			<div class="sponsor-cnt">
+				<img src="images/logo.png">
+				<img src="images/logo-utpl.png">
+			</div>
 		</section>
 
 		<footer class="piePagina">
@@ -74,4 +98,5 @@
 		</footer>
 	</div>
 </body>
+
 </html>
