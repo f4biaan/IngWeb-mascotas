8518f6ac297eacd9463d2d32ed74588e85db8ce5
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-11-09 05:58:24 -0500

    Create productos.php

---
+++ internas/productos.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+	<link rel="icon" href="images/logo.png" sizes="192x192" />
+	<link rel="apple-touch-icon" href="images/logo.png" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+
+<body>
+	<div class="contenedor">
+		<header class="cabeceraPrincipal">
+			<section class="logotipo"><img src="images/logotipoPet.png"></section>
+			<nav class="menuPrincipal">
+				<a href="../index.php">Inicio</a>
+				<a href="servicios.php">Servicios</a>
+				<a href="#">Productos</a>
+				<a href="adopcion.php">Adopción</a>
+				<a href="contactos.php">Contactos</a>
+			</nav>
+		</header>
+
+		<main>
+			<h2>servicios municipio de loja</h2>
+		</main>
+
+		<section class="sliderPrincipal">
+			<img src="images/slider1.jpg">
+		</section>
+
+		<section class="titulo">
+			<h2 class="colorAzul espacio mayusculas">Adopción</h2>
+		</section>
+
+		<section class="adopcion1">
+			<img src="images/slider2.jpg" title="Casimiro" description="perro en adopcion">
+			<section class="card1">
+				<h3 class="mayusculas colorAzul">Casimiro</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet mi nisi, vitae maximus neque laoreet sit amet. Vestibulum sed lacus eu justo convallis ultricies ut et sapien. Sed sed risus non arcu lobortis lacinia quis in elit. Duis cursus nunc a enim iaculis cursus. Nulla sed sem at turpis.</p>
+				<div class="derecha">
+					<button class="button button1">Mas información</button>
+				</div>
+			</section>
+		</section>
+
+		<section class="adopcion2">
+			<img src="images/slider3.jpg" title="garto en adopcion" description="gatp en adopcion">
+			<section class="card1">
+				<h3 class="mayusculas colorAzul">Thomas</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet mi nisi, vitae maximus neque laoreet sit amet. Vestibulum sed lacus eu justo convallis ultricies ut et sapien. Sed sed risus non arcu lobortis lacinia quis in elit. Duis cursus nunc a enim iaculis cursus. Nulla sed sem at turpis.</p>
+				<div class="derecha">
+					<button class="button button1">Mas información</button>
+				</div>
+			</section>
+		</section>
+
+		<section class="doctor">
+			<h3>Doctores</h3>
+		</section>
+
+		<section class="sponsor">
+			<h3>Sponsor</h3>
+		</section>
+
+
+		<footer class="piePagina">
+			<section class="derechos">
+				<h6>Derechos Reservados UTPL 2022</h6>
+			</section>
+			<nav class="redesSociales">
+				<a href=""><i class="fa-brands fa-facebook"></i></a>
+				<a href=""><i class="fa-brands fa-square-instagram"></i></a>
+				<a href=""><i class="fa-brands fa-tiktok"></i></a>
+			</nav>
+		</footer>
+	</div>
+</body>
+
+</html>
